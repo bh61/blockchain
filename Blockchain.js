bb2e050be0fc9e1fb86fd51b2f89772e879e1ae2
bh61 <2186894076@qq.com>
2025-10-16 09:37:53 +0800

    1

--- Blockchain.js
+++ Blockchain.js
@@ -1,25 +1,28 @@
-//引入Block类，用于创建区块对象
+// 引入Block类，用于创建区块对象
 const Block = require('./Block.js');
-//引入node.js的crypto模块，用于SHA-256加密哈希算法
+// 引入node.js的crypto模块，用于SHA-256加密哈希计算
 const crypto = require('crypto');
 
-//创建区块链类--管理整个区块链的核心类
+// 创建区块链类---管理整个区块链的核心类
 class Blockchain {
 
-    //初始化一个新的区块链
+    // 初始化一个新的区块链
     constructor() {
-        this.blockchain = [Block.genesis];          //初始化区块链中的第一个区块
-        this.difficulty = 3;                         //设置挖矿的难度，控制哈希的生成难度
+        this.blockchain = [Block.genesis];  // 初始化区块链中的第一个区块
+        this.difficulty = 3;  // 难度值，控制哈希的生成难度
     }
- // 获取区块链中的所有区块数组，[{0}, {1}, {2}, {3},.....]
+
+    // 获取区块链中的所有区块数组，[{0}, {1}, {2}, {3},.....]
     get() {
         return this.blockchain;
     }
-// 获取区块链中最新的区块（整条链的最末端）
+    
+    // 获取区块链中最新的区块（整条链的最末端）
     get latestBlock() {
         return this.blockchain[this.blockchain.length - 1];
     }
-//验证哈希 值是否满足难度要求的函数
+    
+    // 验证哈希值是否满足难度要求的函数
     isValidHashDifficulty(hash) {
         for (var i = 0; i < hash.length; i++) {
             if (hash[i] !== '0') {
@@ -28,7 +31,7 @@ class Blockchain {
         }
         return i >= this.difficulty;
     }
-//为一个区块对象计算哈希值
+    //为一个区块对象计算哈希值
     calculateHashForBlock(block) {
         const {
             index,
@@ -39,17 +42,12 @@ class Blockchain {
         } = block;
         return this.calculateHash(index, previousHash, timestamp, data, nonce);
     }
-//通用计算哈希值函数的方法
+    //通用的计算哈希值函数的方法
     calculateHash(index, previousHash, timestamp, data, nonce) {
         return crypto.createHash('sha256').update(index + previousHash + timestamp + data + nonce).digest('hex');
     }
-//挖矿方法--创建新的区块并添加到区块链中
+    //挖矿方法--创建新的区块并添加到区块链中
     mine(data) {
-        // const nextIndex = this.latestBlock.index + 1;
-        // const previousHash = this.latestBlock.hash;
-        // let timestamp = new Date().getTime();
-        // let nonce = 0;
-        // let nextHash = this.calculateHash(nextIndex, previousHash, timestamp, data, nonce);
         const newBlock = this.generateNextBlock(data);
         try {
             this.addBlock(newBlock);
@@ -57,7 +55,7 @@ class Blockchain {
             throw error;
         }
     }
-//生成下一个区块的方法--执行工作量证明（挖矿）
+    //生成下一个区块的方法---执行工作量证明（挖矿）
     generateNextBlock(data) {
         const nextIndex = this.latestBlock.index + 1;
         const previousHash = this.latestBlock.hash;
@@ -76,14 +74,14 @@ class Blockchain {
             previousHash,
             timestamp,
             data,
-            nonce,
-            nextHash
+            nextHash,
+            nonce
         );
 
         return nextBlock;
 
     }
-//添加区块到区块链的方法
+    //添加区块到区块链的方法
     addBlock(newBlock) {
         if (this.isValidNextBlock(newBlock, this.latestBlock)) {
             this.blockchain.push(newBlock);
@@ -91,13 +89,13 @@ class Blockchain {
             throw "Error: Invalid Block";
         }
     }
-//验证新情况是否是前一个区块的有效后继
+    //验证新区块是否是前一个区块的有效后继
     isValidNextBlock(nextBlock, previousBlock) {
         const nextBlockHash = this.calculateHashForBlock(nextBlock);
 
         if (previousBlock.index + 1 !== nextBlock.index) {
             return false;
-        } else if (previousBlock.hash !== nextBlock.index) {
+        } else if (previousBlock.hash !== nextBlock.previousHash) {
             return false;
         } else if (nextBlockHash !== nextBlock.hash) {
             return false;
@@ -129,11 +127,11 @@ class Blockchain {
     isChainLonger(chain) {
         return chain.length > this.blockchain.length;
     }
- //替换区块链的方法，---用于处理区块链分叉，保留最长的有效链
+    //替换区块链的方法，---用于处理区块链分叉，保留最长的有效链
     replaceChain(newChain) {
         if (this.isValidChain(newChain) && this.isChainLonger(newChain)) {
             this.blockchain = JSON.parse(JSON.stringify(newChain));
-        }else {
+        } else {
             throw "Error: Invalid Chain";
         }
     }
